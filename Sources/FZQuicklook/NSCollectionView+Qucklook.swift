44223af3436ecee8fc8b2bf1856e370fea08578d
Florian Zand <florianzand@me.com>
2023-06-23 10:08:51 +0200

    updated

--- Sources/FZQuicklook/NSCollectionView+Qucklook.swift
+++ Sources/FZQuicklook/NSCollectionView+Qucklook.swift
@@ -7,77 +7,55 @@
 
 #if os(macOS)
 import AppKit
+import FZSwiftUtils
 
 public extension NSCollectionView {
-    func quicklookItems(at indexPaths: [IndexPath], currentIndexPath: IndexPath? = nil) {
-        guard let dataSource = dataSource else { return }
-        var currentItem: NSCollectionViewItem?
-        let items = indexPaths.compactMap { dataSource.collectionView(self, itemForRepresentedObjectAt: $0) }
-        if let currentIndexPath = currentIndexPath {
-            currentItem = dataSource.collectionView(self, itemForRepresentedObjectAt: currentIndexPath)
+    var quicklookSelectedItemsEnabled: Bool {
+        get { getAssociatedValue(key: "NSCollectionItem_quicklookSelectedItemsEnabled", object: self, initialValue: false) }
+        set {  set(associatedValue: newValue, key: "NSCollectionItem_quicklookSelectedItemsEnabled", object: self)
+            if newValue == true {
+                Self.swizzleCollectionViewResponderEvents()
+            }
         }
-        quicklookItems(items, currentItem: currentItem)
     }
-
-    func qlPreviewable(for item: NSCollectionViewItem) -> QLPreviewable? {
-        guard let indexPath = indexPath(for: item) else { return nil }
-        if let qlItem = (dataSource as? PreviewableDataSource)?.qlPreviewable(for: indexPath) {
-            return qlItem
-        }
-        return item as? QLPreviewable
-    }
-
-    func quicklookItems(_ items: [NSCollectionViewItem], currentItem: NSCollectionViewItem? = nil) {
-        var index = 0
-        var previewItems: [QLPreviewable] = []
-        for item in items {
-            if let qlPreviewable = qlPreviewable(for: item) {
-                previewItems.append(qlPreviewable)
-                if item == currentItem {
-                    index = previewItems.count - 1
+    
+    func quicklookItems(at indexPaths: [IndexPath], current: IndexPath? = nil) {
+        var previewables: [QLPreviewable] = []
+        var currentIndex = 0
+        for indexPath in indexPaths {
+            if let previewable = self.qlPreviewable(for: indexPath) {
+                previewables.append(previewable)
+                if indexPath == current {
+                    currentIndex = previewables.count - 1
                 }
             }
         }
-        /*
-         let items = items.filter({($0 as? QLPreviewable) != nil})
-         let  quicklookItems = items.compactMap({ $0 as? QLPreviewable})
-         guard quicklookItems.isEmpty == false else { return }
-         var index: Int = 0
-         if let currentItem = currentItem {
-             index =  items.firstIndex(of: currentItem) ?? 0
-         }
-         */
-        // let transitionImage = previewItems[index].previewItemTransitionImage ?? items[index].view.renderedImage
+        
         QuicklookPanel.shared.keyDownResponder = self
-        QuicklookPanel.shared.present(previewItems, currentItemIndex: index)
+        QuicklookPanel.shared.present(previewables, currentItemIndex: currentIndex)
     }
 
-    func quicklookSelectedItems(currentItem: NSCollectionViewItem? = nil) {
+    func quicklookItems(_ items: [NSCollectionViewItem], current: NSCollectionViewItem? = nil) {
+        let indexPaths = items.compactMap({self.indexPath(for: $0)})
+        var currentIndexPath: IndexPath? = nil
+        if let current = current {
+            currentIndexPath = self.indexPath(for: current)
+        }
+        self.quicklookItems(at: indexPaths, current: currentIndexPath)
+    }
+
+    func quicklookSelectedItems() {
         let selectedItems = selectionIndexPaths.compactMap { self.item(at: $0) }
-        quicklookItems(selectedItems, currentItem: currentItem)
+        quicklookItems(selectedItems, current: selectedItems.first)
     }
-}
-
-public protocol PreviewableDataSource {
-    func qlPreviewable(for indexPath: IndexPath) -> QLPreviewable?
-}
-
-extension PreviewableDataSource {
-    func qlPreviewable(for indexPaths: [IndexPath]) -> [QLPreviewable] {
-        indexPaths.compactMap { self.qlPreviewable(for: $0) }
-    }
-}
-
-extension NSCollectionViewDiffableDataSource: PreviewableDataSource where ItemIdentifierType: QLPreviewable {
-    public func qlPreviewable(for indexPath: IndexPath) -> QLPreviewable? {
-        itemIdentifier(for: indexPath)
-    }
-}
-
-@available(macOS 11.0, *)
-extension NSTableViewDiffableDataSource: PreviewableDataSource where ItemIdentifierType: QLPreviewable {
-    public func qlPreviewable(for indexPath: IndexPath) -> QLPreviewable? {
-        itemIdentifier(forRow: indexPath.item)
+    
+    internal func qlPreviewable(for indexPath: IndexPath) -> QLPreviewable? {
+        if let dataSource = self.dataSource as? (PreviewableDataSource & NSCollectionViewDataSource) {
+            return dataSource.qlPreviewable(for: indexPath)
+        } else if let dataSource = self.dataSource {
+            return dataSource.collectionView(self, itemForRepresentedObjectAt: indexPath) as? QLPreviewable
+        }
+        return nil
     }
 }
 
