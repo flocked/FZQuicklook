256d035977b2e3d691c38b54cd78a5c1d9b421d7
Florian Zand <florianzand@me.com>
2023-07-01 07:09:00 +0200

    updated

--- Sources/FZQuicklook/Extensions/NSCollectionView+KeyDown.swift
+++ Sources/FZQuicklook/Extensions/NSCollectionView+KeyDown.swift
@@ -14,6 +14,11 @@ internal extension NSCollectionView {
         set {  set(associatedValue: newValue, key: "NSCollectionView_keyDownMonitor", object: self) }
     }
     
+    var mouseDownMonitor: Any? {
+        get { getAssociatedValue(key: "NSCollectionView_mouseDownMonitor", object: self, initialValue: nil) }
+        set {  set(associatedValue: newValue, key: "NSCollectionView_mouseDownMonitor", object: self) }
+    }
+    
     func setupKeyDownMonitor() {
         if isQuicklookPreviewable {
             guard keyDownMonitor == nil else { return }
@@ -22,6 +27,9 @@ internal extension NSCollectionView {
                 if self.isQuicklookPreviewable, event.keyCode == 49 {
                     if QuicklookPanel.shared.isVisible == false {
                         self.quicklookSelectedItems()
+                        if QuicklookPanel.shared.isVisible == true {
+                            self.setupMouseDownMonitor()
+                        }
                     }
                 } else {
                     if QuicklookPanel.shared.isVisible {
@@ -38,6 +46,29 @@ internal extension NSCollectionView {
         } else if let keyDownMonitor = self.keyDownMonitor {
             NSEvent.removeMonitor(keyDownMonitor)
             self.keyDownMonitor = nil
+        }
+    }
+    
+    func setupMouseDownMonitor() {
+        Swift.print("setupMouseDownMonitor")
+        if isQuicklookPreviewable, QuicklookPanel.shared.isVisible {
+            guard mouseDownMonitor == nil else { return }
+            mouseDownMonitor = NSEvent.addLocalMonitorForEvents(matching: .leftMouseDown, handler: { [weak self] event in
+                guard let self = self else { return event }
+                Swift.print("mouseMonitor window", event.window ?? "")
+                if let contentView = event.window?.contentView {
+                    let location = event.location(in: contentView)
+                    if let hitView = contentView.hitTest(location) {
+                        Swift.print("mouseMonitor hitview", hitView)
+                    }
+                    
+                }
+
+                return event
+            })
+        } else if let mouseDownMonitor = self.mouseDownMonitor {
+            NSEvent.removeMonitor(mouseDownMonitor)
+            self.mouseDownMonitor = nil
         }
     }
 }
