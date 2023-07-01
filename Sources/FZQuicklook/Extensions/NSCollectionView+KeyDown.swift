aed5eca44fd195d84811e4a75e15bdf87e43178e
Florian Zand <florianzand@me.com>
2023-07-01 06:02:43 +0200

    Update NSCollectionView+KeyDown.swift

--- Sources/FZQuicklook/Extensions/NSCollectionView+KeyDown.swift
+++ Sources/FZQuicklook/Extensions/NSCollectionView+KeyDown.swift
@@ -21,7 +21,7 @@ internal extension NSCollectionView {
             }
         } else {
             let previousSelectionIndexPaths = self.selectionIndexPaths
-         //   self.swizzledKeyDown(with: event)
+            self.keyDown(with: event)
             if QuicklookPanel.shared.isVisible, selectionIndexPaths != previousSelectionIndexPaths {
                 self.quicklookSelectedItems()
             }
