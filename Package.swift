f63716db5da618a4ee1eb84d04d1d3c063eaaa8a
Florian Zand <florianzand@me.com>
2025-05-05 04:30:03 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FZQuicklook",
-    platforms: [.macOS("10.15.1")],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(
             name: "FZQuicklook",
